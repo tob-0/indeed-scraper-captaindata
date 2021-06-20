893175c9d67a32b3ab436d94fb375248c542fb3a
tob-0 <tbsx@outlook.fr>
2021-06-20 20:42:20 +0200

    WhatWhere form handling

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const puppeteer = require('puppeteer')
 
 run = async _ => {
-    const browser = await puppeteer.launch({headless: false}) // For debugging, allows to see what's happening 
+    const browser = await puppeteer.launch({headless: false,slowMo:250}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
     const page = await browser.newPage()
 
     await page.goto('https://fr.indeed.com/', {
         waitUntil: 'networkidle2',
     }) // Go to indeed's website
-    // do sumthing
+
+
+    // WhatWhere Form handling
+    await page.$eval('#text-input-what', what => what.value = 'Developpeur Python') // Set the "What"
+    await page.$eval('#text-input-where', where => where.value = 'Paris (75)') // Set the "Where"
+    await page.click('button.icl-WhatWhere-button') // Submit the search
+
     await browser.close()
 }
 
