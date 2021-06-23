7926be02e674297b8545663e1de9df15ff0d13d8
tob-0 <tbsx@outlook.fr>
2021-06-23 15:30:50 +0200

    [refacto] Changement approche

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const puppeteer = require('puppeteer');
 
 (async () =>{
-    const browser = await puppeteer.launch({headless: false,slowMo:250}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
+    const browser = await puppeteer.launch({headless: false,slowMo:1}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
     const page = await browser.newPage()
-    await page.goto('https://fr.indeed.com/', {
+    await page.goto('https://fr.indeed.com/jobs?q=Python&l=Paris%20(75)', {
         waitUntil: 'networkidle2',
     }) // Go to indeed's website
 
-
+    /*
+        Au final, je vais direct sur la page de la recherche, parce aue modifie l'attribut value de l'input n'a pas l'air de marcher
+        J'aurais pu faire un 'keyboard.type' pour ecrire dedans, mais je trouvais pas ca tres elegant, 
+            donc a la place ce sera une modf des params de l'URL 'q' & 'l'
+            
     // WhatWhere Form handling
     await page.$eval('#text-input-what', what => what.value = 'Developpeur Python') // Set the "What"
     await page.$eval('#text-input-where', where => where.value = 'Paris (75)') // Set the "Where"
     await page.click('button.icl-WhatWhere-button') // Submit the search
+    */
 
     await browser.close()
 })()
