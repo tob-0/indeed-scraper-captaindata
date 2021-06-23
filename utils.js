6edfebe921ad0a64ee63039413b94f4429af9e44
tob-0 <tbsx@outlook.fr>
2021-06-23 20:25:30 +0200

    Ajout des autres fonctions communes

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 module.exports = {
     importJSON: (path) => {
             try {
@@ -5,5 +7,16 @@ module.exports = {
             } catch (err) {
                 console.error(err)
             }
-        }
+    },
+    exportToJSON: (data,path) => {
+        let jsonifiedData = JSON.stringify(data)
+        fs.writeFile(path,jsonifiedData,'utf8',e=> {
+            if (e) return e
+        })
+        return true
+    },
+    compareData:(d0,d1)=>{
+        let concatData = d0.concat(d1)
+        return concatData.filter((value,index,self)=>self.indexOf(value) === index)
+    }
 }
