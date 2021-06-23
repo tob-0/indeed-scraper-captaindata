522e9672862e705a44c87e219c546228523cbd27
tob-0 <tbsx@outlook.fr>
2021-06-23 20:26:25 +0200

    Liste d'objets avec job title et description

--- getJobsInfo.js
+++ getJobsInfo.js
@@ -4,12 +4,23 @@ const utils = require('./utils')
 let storedData = utils.importJSON('bot-links.json')
 
 ;(async (links) =>{
-    const browser = await puppeteer.launch({headless: true,slowMo:1}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
+    const browser = await puppeteer.launch({headless: true,slowMo:0})
     const page = await browser.newPage()
+    let jobObjects = []
     for (const link of links) {
         await page.goto(link, {
             waitUntil: 'networkidle2',
         })
-        
+        await page.waitForSelector('.jobsearch-JobInfoHeader-title')
+        const jobTitle = await page.$eval('.jobsearch-JobInfoHeader-title', e => e.innerHTML)
+        const jobDesc = await page.$eval('#jobDescriptionText', e => e.innerHTML)
+        console.log(jobTitle)
+        console.log(jobDesc)
+        jobObjects.push({
+            title: jobTitle,
+            desc: jobDesc
+        })
+
     }
+    await browser.close()
 })(storedData)
