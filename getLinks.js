61419199a83acf40815fad9160a8cc2ae51f7caa
tob-0 <tbsx@outlook.fr>
2021-06-23 17:48:46 +0200

    Fonction pour l'enregistrement en JSON

--- getLinks.js
+++ getLinks.js
@@ -1,16 +1,31 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const fs = require('fs')
 
-function urlFormatter(q,l,advn,vjk) {`https://fr.indeed.com/jobs?q=${q}&l=${l}&advn=${advn}&vjk=${vjk}`}
+function urlFormatter(q,l,advn,vjk) {
+    return advn===null ? encodeURI(`https://fr.indeed.com/jobs?q=${q}&l=${l}&vjk=${vjk}`) : encodeURI(`https://fr.indeed.com/jobs?q=${q}&l=${l}&advn=${advn}&vjk=${vjk}`)
+}
+
+function exportToJSON(data) {
+    let jsonifiedData = JSON.stringify(data)
+    fs.writeFile('links.json',jsonifiedData,'utf8',e=> {
+        if (e) return e
+    })
+    return true
+}
+
+function importJSON(path) {
+    return JSON.parse(fs.readFileSync(path,'utf8'))
+}
 
 (async () =>{
-    const browser = await puppeteer.launch({headless: false,slowMo:1}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
+    const browser = await puppeteer.launch({headless: true,slowMo:1}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
     const page = await browser.newPage()
     await page.goto('https://fr.indeed.com/jobs?q=Python&l=Paris%20(75)', {
         waitUntil: 'networkidle2',
     }) // Go to indeed's website
 
     /*
-        Au final, je vais direct sur la page de la recherche, parce aue modifie l'attribut value de l'input n'a pas l'air de marcher
+        Au final, je vais direct sur la page de la recherche, parce que modifier l'attribut value de l'input n'a pas l'air de marcher
         J'aurais pu faire un 'keyboard.type' pour ecrire dedans, mais je trouvais pas ca tres elegant, 
             donc a la place ce sera une modf des params de l'URL 'q' & 'l'
 
@@ -19,11 +34,8 @@ function urlFormatter(q,l,advn,vjk) {`https://fr.indeed.com/jobs?q=${q}&l=${l}&a
     await page.$eval('#text-input-where', where => where.value = 'Paris (75)') // Set the "Where"
     await page.click('button.icl-WhatWhere-button') // Submit the search
     */
-    try {
-        await page.waitForSelector('div.jobsearch-SerpJobCard')
-        const vjkDataAdvn = await page.$$eval('div.jobsearch-SerpJobCard', e =>e.map(x=>[x.getAttribute('data-jk'), x.getAttribute('data-empn')]))
-    } catch (error) {
-        console.log(error)
-    }
+    await page.waitForSelector('div.jobsearch-SerpJobCard')
+    const jobsData = await page.$$eval('div.jobsearch-SerpJobCard', e =>e.map(x=>[x.getAttribute('data-empn'),x.getAttribute('data-jk')]))
+    jobsData.forEach(jobData=>console.log(urlFormatter('Python','Paris%20(75)',jobData[0],jobData[1])))
     await browser.close()
 })()
