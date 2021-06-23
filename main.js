d41b6624985ebfc482abfca00557362cc5089635
tob-0 <tbsx@outlook.fr>
2021-06-23 16:29:49 +0200

    [add] Grep des clef de jobs

--- main.js
+++ main.js
@@ -11,12 +11,19 @@ const puppeteer = require('puppeteer');
         Au final, je vais direct sur la page de la recherche, parce aue modifie l'attribut value de l'input n'a pas l'air de marcher
         J'aurais pu faire un 'keyboard.type' pour ecrire dedans, mais je trouvais pas ca tres elegant, 
             donc a la place ce sera une modf des params de l'URL 'q' & 'l'
-            
+
     // WhatWhere Form handling
     await page.$eval('#text-input-what', what => what.value = 'Developpeur Python') // Set the "What"
     await page.$eval('#text-input-where', where => where.value = 'Paris (75)') // Set the "Where"
     await page.click('button.icl-WhatWhere-button') // Submit the search
     */
-
-    await browser.close()
+    try {
+        await page.waitForSelector('div.jobsearch-SerpJobCard')
+        const vjkData = await page.$$eval('div.jobsearch-SerpJobCard', e =>e.map(x=>x.getAttribute('data-jk')))
+        const empnData = await page.$$eval('div.jobsearch-SerpJobCard', e=>e.map(x=>x.getAttribute('data-empn')))
+    } catch (error) {
+        console.log(error)
+    }
+    
+    // await browser.close()
 })()
