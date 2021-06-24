67ce727fa4bcfc24daff51596103b48e98e8bc7c
tob-0 <tbsx@outlook.fr>
2021-06-24 14:40:47 +0200

    API fetch pour des infos supplementaires

--- getJobsInfo.js
+++ getJobsInfo.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer')
 const utils = require('./utils')
-
+const fetch = require('node-fetch')
 
 module.exports = {
     /**
@@ -10,7 +10,6 @@ module.exports = {
      * @param {boolean} CSVExport Set to 'true' if you want to export to csv
      */
     getInfo: async (links,CSVExport) =>{
-        CSVExport
         const browser = await puppeteer.launch({headless: true,slowMo:0})
         const page = await browser.newPage()
         let jobs = {
@@ -18,19 +17,32 @@ module.exports = {
             items:[]
         }
         for (const link of links) {
-            await page.goto(link, {
+            let linkAPI = link[1]
+            await page.goto(link[0], {
                 waitUntil: 'networkidle2',
             })
             await page.waitForSelector('.jobsearch-JobInfoHeader-title')
             const jobTitle = await page.$eval('.jobsearch-JobInfoHeader-title', e => e.innerHTML)
             const jobDesc = await page.$eval('#jobDescriptionText', e => e.innerHTML)
-            jobs.items.push({
+            let jobObject = {
                 title: jobTitle,
                 desc_html: jobDesc,
                 desc_md: utils.convertHTML(jobDesc,true),
                 desc_text: utils.convertHTML(jobDesc,false),
-                link: link
-            })
+                link: link[0]
+            }
+            ;(async (jobObj,link)=>{
+                let data = await fetch(link).then(resp=>resp.json()).then(data=>data).catch(err=>console.log(err.message))
+                jobObj.jobLocation = data.jobLocation
+                jobObj.contractTypes = data.jtsT
+                jobObj.salaryRange = data.ssT
+                jobObj.salaryMin = data.sEx.sRg.split(' - ')[0]
+                jobObj.salaryMax = data.sEx.sRg.split(' - ')[1]
+                jobObj.age = data.relativeJobAge
+                
+                jobs.items.push(jobObj)
+
+            })(jobObject, linkAPI)
     
         }
         await browser.close()
