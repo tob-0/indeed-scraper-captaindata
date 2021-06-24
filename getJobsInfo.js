18dba0a43d34e73e49ef44cafd15303265d48aa2
tob-0 <tbsx@outlook.fr>
2021-06-24 11:07:57 +0200

    Changement de la structure du json

--- getJobsInfo.js
+++ getJobsInfo.js
@@ -3,10 +3,14 @@ const utils = require('./utils')
 
 let storedLinks = utils.importJSON('bot-links.json')
 
-;(async (links) =>{
+;(async (links,CSVExport) =>{
+    CSVExport
     const browser = await puppeteer.launch({headless: true,slowMo:0})
     const page = await browser.newPage()
-    let jobObjects = []
+    let jobs = {
+        count: 0,
+        items:[]
+    }
     for (const link of links) {
         await page.goto(link, {
             waitUntil: 'networkidle2',
@@ -14,22 +18,23 @@ let storedLinks = utils.importJSON('bot-links.json')
         await page.waitForSelector('.jobsearch-JobInfoHeader-title')
         const jobTitle = await page.$eval('.jobsearch-JobInfoHeader-title', e => e.innerHTML)
         const jobDesc = await page.$eval('#jobDescriptionText', e => e.innerHTML)
-        console.log(jobTitle)
-        console.log(jobDesc)
-        jobObjects.push({
+        jobs.items.push({
             title: jobTitle,
-            desc: [
-                jobDesc,
-                utils.convertHTML(jobDesc,true),
-                utils.convertHTML(jobDesc,false)
-            ],
+            desc_html: jobDesc,
+            desc_md: utils.convertHTML(jobDesc,true),
+            desc_text: utils.convertHTML(jobDesc,false),
             link: link
         })
 
     }
     await browser.close()
+    jobs.count = jobs.items.length
     let storedData = utils.importJSON('bot-jobs.json')
-    let uniqData = utils.compareData(storedData, jobObjects)
-    utils.exportToJSON(uniqData, 'bot-jobs.json')
+    let uniqData = utils.compareData(storedData.items, jobs.items)
+    jobs.items = uniqData
+    utils.exportToJSON(jobs, 'bot-jobs.json')
 
-})(storedLinks)
+    
+    if (CSVExport) utils.exportToCSV(uniqData.items,'bot-jobs.csv') // Ne fonctionne pas, je suis pas sur de pourquoi mais j'ai pas trop le temps de debug
+
+})(storedLinks,true)
