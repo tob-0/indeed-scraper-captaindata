32965f0da8b8ec7c3e88823f957dc0ffdd74aab5
tob-0 <tbsx@outlook.fr>
2021-06-24 11:37:32 +0200

    Passage en mode module

--- getJobsInfo.js
+++ getJobsInfo.js
@@ -1,40 +1,41 @@
 const puppeteer = require('puppeteer')
 const utils = require('./utils')
 
-let storedLinks = utils.importJSON('bot-links.json')
 
-;(async (links,CSVExport) =>{
-    CSVExport
-    const browser = await puppeteer.launch({headless: true,slowMo:0})
-    const page = await browser.newPage()
-    let jobs = {
-        count: 0,
-        items:[]
-    }
-    for (const link of links) {
-        await page.goto(link, {
-            waitUntil: 'networkidle2',
-        })
-        await page.waitForSelector('.jobsearch-JobInfoHeader-title')
-        const jobTitle = await page.$eval('.jobsearch-JobInfoHeader-title', e => e.innerHTML)
-        const jobDesc = await page.$eval('#jobDescriptionText', e => e.innerHTML)
-        jobs.items.push({
-            title: jobTitle,
-            desc_html: jobDesc,
-            desc_md: utils.convertHTML(jobDesc,true),
-            desc_text: utils.convertHTML(jobDesc,false),
-            link: link
-        })
-
-    }
-    await browser.close()
-    jobs.count = jobs.items.length
-    let storedData = utils.importJSON('bot-jobs.json')
-    let uniqData = utils.compareData(storedData.items, jobs.items)
-    jobs.items = uniqData
-    utils.exportToJSON(jobs, 'bot-jobs.json')
-
+module.exports = {
+    getInfo: async (links,CSVExport) =>{
+        CSVExport
+        const browser = await puppeteer.launch({headless: true,slowMo:0})
+        const page = await browser.newPage()
+        let jobs = {
+            count: 0,
+            items:[]
+        }
+        for (const link of links) {
+            await page.goto(link, {
+                waitUntil: 'networkidle2',
+            })
+            await page.waitForSelector('.jobsearch-JobInfoHeader-title')
+            const jobTitle = await page.$eval('.jobsearch-JobInfoHeader-title', e => e.innerHTML)
+            const jobDesc = await page.$eval('#jobDescriptionText', e => e.innerHTML)
+            jobs.items.push({
+                title: jobTitle,
+                desc_html: jobDesc,
+                desc_md: utils.convertHTML(jobDesc,true),
+                desc_text: utils.convertHTML(jobDesc,false),
+                link: link
+            })
     
-    if (CSVExport) utils.exportToCSV(jobs.items,'bot-jobs.csv')
-
-})(storedLinks,true)
+        }
+        await browser.close()
+        jobs.count = jobs.items.length
+        let storedData = utils.importJSON('bot-jobs.json')
+        let uniqData = utils.compareData(storedData.items, jobs.items)
+        jobs.items = uniqData
+        utils.exportToJSON(jobs, 'bot-jobs.json')
+    
+        
+        if (CSVExport) utils.exportToCSV(jobs.items,'bot-jobs.csv')
+    
+    }
+}
--- getLinks.js
+++ getLinks.js
@@ -5,31 +5,33 @@ function urlFormatter(q,l,advn,vjk) {
     return advn===null ? encodeURI(`https://fr.indeed.com/jobs?q=${q}&l=${l}&vjk=${vjk}`) : encodeURI(`https://fr.indeed.com/jobs?q=${q}&l=${l}&advn=${advn}&vjk=${vjk}`)
 }
 
-(async (q,l) =>{
-    const browser = await puppeteer.launch({headless: true,slowMo:1}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
-    const page = await browser.newPage()
-    q = encodeURI(q),l=encodeURI(l)
-    await page.goto(`https://fr.indeed.com/jobs?q=${q}&l=${l})`, {
-        waitUntil: 'networkidle2',
-    }) // Go to indeed's website
+module.exports = {
+    getLinks: async (q,l) =>{
+        const browser = await puppeteer.launch({headless: true,slowMo:1}) // For debugging, allows to see what's happening + Slow motion (delay by 250ms)
+        const page = await browser.newPage()
+        q = encodeURI(q),l=encodeURI(l)
+        await page.goto(`https://fr.indeed.com/jobs?q=${q}&l=${l})`, {
+            waitUntil: 'networkidle2',
+        }) // Go to indeed's website
 
-    /*
-        Au final, je vais direct sur la page de la recherche, parce que modifier l'attribut value de l'input n'a pas l'air de marcher
-        J'aurais pu faire un 'keyboard.type' pour ecrire dedans, mais je trouvais pas ca tres elegant, 
-            donc a la place ce sera une modf des params de l'URL 'q' & 'l'
+        /*
+            Au final, je vais direct sur la page de la recherche, parce que modifier l'attribut value de l'input n'a pas l'air de marcher
+            J'aurais pu faire un 'keyboard.type' pour ecrire dedans, mais je trouvais pas ca tres elegant, 
+                donc a la place ce sera une modf des params de l'URL 'q' & 'l'
 
-    // WhatWhere Form handling
-    await page.$eval('#text-input-what', what => what.value = 'Developpeur Python') // Set the "What"
-    await page.$eval('#text-input-where', where => where.value = 'Paris (75)') // Set the "Where"
-    await page.click('button.icl-WhatWhere-button') // Submit the search
-    */
-    await page.waitForSelector('div.jobsearch-SerpJobCard')
-    const jobsData = await page.$$eval('div.jobsearch-SerpJobCard', e =>e.map(x=>[x.getAttribute('data-empn'),x.getAttribute('data-jk')]))
-    await browser.close()
+        // WhatWhere Form handling
+        await page.$eval('#text-input-what', what => what.value = 'Developpeur Python') // Set the "What"
+        await page.$eval('#text-input-where', where => where.value = 'Paris (75)') // Set the "Where"
+        await page.click('button.icl-WhatWhere-button') // Submit the search
+        */
+        await page.waitForSelector('div.jobsearch-SerpJobCard')
+        const jobsData = await page.$$eval('div.jobsearch-SerpJobCard', e =>e.map(x=>[x.getAttribute('data-empn'),x.getAttribute('data-jk')]))
+        await browser.close()
 
-    let jobsLinks = []
-    jobsData.forEach(jobData=>jobsLinks.push(urlFormatter(q,l,jobData[0],jobData[1])))
-    let storedData = utils.importJSON('bot-links.json')
-    let jobsLinksUniq = utils.compareData(storedData, jobsLinks)
-    utils.exportToJSON(jobsLinksUniq,'bot-links.json')
-})('Python','Paris (75)')
+        let jobsLinks = []
+        jobsData.forEach(jobData=>jobsLinks.push(urlFormatter(q,l,jobData[0],jobData[1])))
+        let storedData = utils.importJSON('bot-links.json')
+        let jobsLinksUniq = utils.compareData(storedData, jobsLinks)
+        utils.exportToJSON(jobsLinksUniq,'bot-links.json')
+    }
+}
