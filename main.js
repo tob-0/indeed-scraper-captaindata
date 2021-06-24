221ba3364d731881e006d74f0b410f5645eeaf0b
tob-0 <tbsx@outlook.fr>
2021-06-24 11:36:50 +0200

    creation d'un main.js pour tout faire d'un coup

---
+++ main.js
@@ -0,0 +1,9 @@
+const cards = require('./getLinks')
+const jobs = require('./getJobsInfo')
+const utils = require('./utils')
+
+cards.getLinks('Python','Paris (75)')
+
+let storedLinks = utils.importJSON('bot-links.json')
+
+jobs.getInfo(storedLinks)
