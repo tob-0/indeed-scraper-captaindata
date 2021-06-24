8fe2279f093f3159480b84d3e04035a79eecfb9a
tob-0 <tbsx@outlook.fr>
2021-06-24 14:05:20 +0200

    doc + joblinks api

--- getLinks.js
+++ getLinks.js
@@ -1,8 +1,26 @@
 const puppeteer = require('puppeteer')
 const utils = require('./utils')
 
+
+/**
+ * Forge job URL from data
+ * @param {string} q Query
+ * @param {string} l location
+ * @param {string} advn ?
+ * @param {string} vjk JobKey
+ * @returns string
+ */
 function urlFormatter(q,l,advn,vjk) {
     return advn===null ? encodeURI(`https://fr.indeed.com/jobs?q=${q}&l=${l}&vjk=${vjk}`) : encodeURI(`https://fr.indeed.com/jobs?q=${q}&l=${l}&advn=${advn}&vjk=${vjk}`)
+}
+
+/**
+ * Forge API url for specific job
+ * @param {string} jk jobkey
+ * @returns string
+ */
+function urlFormatterAPI(jk) {
+    return `https://fr.indeed.com/viewjob?jk=${jk}&vjs=1`
 }
 
 module.exports = {
@@ -35,7 +53,7 @@ module.exports = {
         await browser.close()
 
         let jobsLinks = []
-        jobsData.forEach(jobData=>jobsLinks.push(urlFormatter(q,l,jobData[0],jobData[1])))
+        jobsData.forEach(jobData=>jobsLinks.push([urlFormatter(q,l,jobData[0],jobData[1]),urlFormatterAPI(jobData[1])]))
         let storedData = utils.importJSON('bot-links.json')
         let jobsLinksUniq = utils.compareData(storedData, jobsLinks)
         utils.exportToJSON(jobsLinksUniq,'bot-links.json')
